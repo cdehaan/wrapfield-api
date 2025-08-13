c4c418f8dad934f33662fb7dd281f8b6279107d9
Chris DeHaan <chris@nextvision.co.jp>
2025-08-13 15:12:28 +0900

    more headers removed from here, set in nginx config

--- public/index.php
+++ public/index.php
@@ -8,15 +8,6 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-// --- CORS (adjust in .env for prod) ---
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
 // Always return JSON
 header('Content-Type: application/json');
 
